1718beee427fc0545865c9fab2f8c273d3efec6a
Lyubov Agulova <lu7623@gmail.com>
2024-10-01 21:29:58 +0300

    feat: hash tasks done

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,23 @@
+import { createReadStream } from "node:fs";
+import { createPath } from "../utils.js";
+const { createHash } = await import("node:crypto");
+
+const filename = createPath("hash/files/fileToCalculateHashFor.txt");
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash("sha256");
+  const readStream = createReadStream(filename);
+
+  readStream.on("data", (data) => {
+    hash.update(data);
+  });
+
+  readStream.on("end", () => {
+    const hashValue = hash.digest("hex");
+    console.log(hashValue);
+  });
+
+  readStream.on("error", (err) => console.error(err.message));
 };
 
 await calculateHash();
